110329c74e8f06f3620ef52a68efebbcfd2b7866
Jason Rivera <jrivera2850@gmail.com>
2019-10-01 21:19:07 -0500

    update app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import { getPokemon, getAllPokemon } from './services/pokemon';
 import './App.css';
 
 function App() {
-  const [pokemonData, setPokemonData] = useState(null);
+  const [pokemonData, setPokemonData] = useState([])
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
   const [loading, setLoading] = useState(true);
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
+
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setPokemonData(data.results)
-        setLoading(false);
-      })
+    async function fetchData() {
+      let data = await getAllPokemon(initialURL)
+      setNextUrl(data.next);
+      setPrevUrl(data.previous);
+      await loadPokemon(data.results);
+      setLoading(false);
+    }
+    fetchData();
   }, [])
 
-  if (loading) {
-    return <h1>Loading</h1>;
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
   }
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const _asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index]);
+    }
+  };
+
+  const _getPokemon = async (pokemon, status) => {
+    let response = await getPokemon(pokemon);
+    return response;
+  };
+
+  const loadPokemon = async (data) => {
+    let _pokemon = []
+
+    await _asyncForEach(data, async pokemon => {
+      let pokemonRecord = await _getPokemon(pokemon)
+      _pokemon.push(pokemonRecord);
+    })
+    setPokemonData(_pokemon);
+  }
+
   return (
     <>
       <Navbar />
       <div className="container">
-        <div className="grid-container">
-          {pokemonData.map((pokemon, i) => {
-            console.log('poke')
-            return <Card key={i} url={pokemon.url} pokemon={pokemon} />
-          })}
-        </div>
+        {loading ? <h1 style={{ textAlign: 'center' }}>Loading...</h1> : (
+          <>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+            <div className="grid-container">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} url={pokemon.url} pokemon={pokemon} />
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,40 +5,51 @@ import './style.css';
 function Card(props) {
     const [pokemonData, setPokemonData] = useState(null);
     const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        fetch(props.url)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setPokemonData(data)
-                setLoading(false);
-            })
-    }, [])
+    // useEffect(() => {
+    //     fetch(props.url)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setPokemonData(data);
+    //             setLoading(false);
+    //         })
+    // }, [])
 
     return (
         <div className="Card">
-            {loading ? <span>Loading</span> : (
-                <>
-                    <div className="Card__img">
-                        <img src={pokemonData.sprites.front_default} alt="" />
-                    </div>
-                    <div className="Card__name">
-                        {pokemonData.name}
-                    </div>
-                    <div className="Card__types">
-                        {
-                            pokemonData.types.map(type => {
-                                return (
-                                    <div className="Card__type" style={{ backgroundColor: t[type.type.name] }}>
-                                        {type.type.name}
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </>
-            )}
+
+
+            <div className="Card__img">
+                <img src={props.pokemon.sprites.front_default} alt="" />
+            </div>
+            <div className="Card__name">
+                {props.pokemon.name}
+            </div>
+            <div className="Card__types">
+                {
+                    props.pokemon.types.map(type => {
+                        return (
+                            <div className="Card__type" style={{ backgroundColor: t[type.type.name] }}>
+                                {type.type.name}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="Card__info">
+                <div className="Card__data Card__data--weight">
+                    <p className="title">Weight</p>
+                    <p>{props.pokemon.weight}</p>
+                </div>
+                <div className="Card__data Card__data--weight">
+                    <p className="title">Height</p>
+                    <p>{props.pokemon.height}</p>
+                </div>
+                <div className="Card__data Card__data--ability">
+                    <p className="title">Ability</p>
+                    <p>{props.pokemon.abilities[0].ability.name}</p>
+                </div>
+            </div>
+
         </div>
     );
 }
