1b5d6703123ddfa6c5685087b8c2757af6cb7f78
Tausif <tausifulhaque539@gmail.com>
2021-10-26 15:39:12 +0530

    Updates

---
+++ precache-manifest.c4026eb7d7d13be26144b8bca1913b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d172698288f3a273a5a45a2586a4d55a",
+    "url": "/pokereact/index.html"
+  },
+  {
+    "revision": "49a3783e83b51ee6c2bf",
+    "url": "/pokereact/static/css/main.3b3416de.chunk.css"
+  },
+  {
+    "revision": "32536be9ddac00e5c2b5",
+    "url": "/pokereact/static/js/2.f23e6dfe.chunk.js"
+  },
+  {
+    "revision": "49a3783e83b51ee6c2bf",
+    "url": "/pokereact/static/js/main.89ac22ea.chunk.js"
+  },
+  {
+    "revision": "416a3463f8176be107f2",
+    "url": "/pokereact/static/js/runtime~main.c703519f.js"
+  }
+]);
